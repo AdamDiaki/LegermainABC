29049f19f9769873cac79f3450866d1b63df203d
Christian-Mb <44323151+Christian-Mb@users.noreply.github.com>
2019-12-28 01:52:37 +0100

    add charpente_show

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Entity\ArticleImages;
 use App\Repository\ArticleImagesRepository;
 use App\Repository\ArticleRepository;
 use App\Repository\ImageRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use phpDocumentor\Reflection\Types\Array_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticlesController extends AbstractController
@@ -24,6 +27,15 @@ class ArticlesController extends AbstractController
         return $this->render( 'articles/charpente.html.twig', [
             'controller_name' => 'ArticlesController',  'articles' =>$articles,
         ] );
+    }
+
+    /**
+     * @Route("/charpente/{id}",name="charpente_show")
+     */
+    public function show(Article $article = null){
+        return $this->render('articles/charpente_show.html.twig',['article' => $article
+
+        ]);
     }
     /**
      * @Route("/couverture", name="couverture")
