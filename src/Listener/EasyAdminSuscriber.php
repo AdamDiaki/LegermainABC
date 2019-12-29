9a51f47edbeb17f22dfe051063945c0211e55735
Christian-Mb <44323151+Christian-Mb@users.noreply.github.com>
2019-12-29 23:57:33 +0100

    send message to the customer

---
+++ src/Listener/EasyAdminSuscriber.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Listener;
+
+use App\Entity\RequestProject;
+use EasyCorp\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+
+class EasyAdminSuscriber implements EventSubscriberInterface
+{
+
+    private $mailer;
+
+    /**
+     * EasyAdminSuscriber constructor.
+     */
+    public function __construct(\Swift_Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+
+
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+          EasyAdminEvents::PRE_UPDATE => array('preEdit'),
+        );
+    }
+
+    public function preEdit(GenericEvent $event){
+        $entity = $event->getSubject();
+
+        if (!($entity instanceof  RequestProject)){
+            return;
+        }
+
+        $message = (new \Swift_Message("Votre de demande de devis a été lu par l'artisan"))
+            ->setFrom('legermainabc@gmail.com')
+            ->setTo($entity->getUser()->getEmail())
+            ->setBody("Bonjour M/Mme ". $entity->getUser()->getName()." ". $entity->getUser()->getFirstname().".". " L'artisan est  entrain d'examiné votre demande devis. Merci de votre confiance !"
+            )
+        ;
+        $this->mailer->send($message);
+
+
+
+
+    }
+
+
+}
