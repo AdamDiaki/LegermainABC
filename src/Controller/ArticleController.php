57abcb543c272dab350c6dd8c87c851c623394ec
adam diakite <adam.diakite@etu.univ-tours.fr>
2020-01-12 19:13:42 +0100

    merge final

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/article", name="article")
+     */
+    public function index()
+    {
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+}
